4c627ebe88377112903069e67386c05d43049a7f
Luca Rinelli <rinelliluca@gmail.com>
2018-12-12 16:39:54 +0000

    Task 2 detecting proibitory

--- task2/main.m
+++ task2/main.m
@@ -62,24 +62,52 @@ for k = 1 : length(theFiles)
     % Find all the blue dark circles in the image
     [centersBlueDark, radiiBlueDark] = imfindcircles(blueMask, [Rmin Rmax],'ObjectPolarity','dark','Sensitivity',0.90);
     
+    % Find all the blue bright circles in the image
+    [centersRedBright, radiiRedBright] = imfindcircles(redMask,[Rmin Rmax],'ObjectPolarity','bright','Sensitivity',0.90);
+    % Find all the blue dark circles in the image
+    [centersRedDark, radiiRedDark] = imfindcircles(redMask, [Rmin Rmax],'ObjectPolarity','dark','Sensitivity',0.90);
+
+    
+    %% MASKS
     if size(centersDark,1)==1 %MAYBE WE DON'T NEED THIS
         circ_mask1 = circularMask(centersDark,radiiDark+2,size(gray));       
     else
-        circ_mask1 = circularMask(size(gray)/2,mean(size(gray)),size(gray));
+        circ_mask1 = circularMask(size(gray)/2,mean(size(gray))/2,size(gray));
     end
+    
     tria_mask3 = roipoly(gray,[size(gray,2)/2 size(gray,2)/4 3*size(gray,2)/4],[size(gray,1)/4 3*size(gray,1)/4 3*size(gray,1)/4]);
     tria_mask2 = roipoly(gray,[size(gray,2)/2 1 size(gray,2)],[1 size(gray,1) size(gray,1)])-tria_mask3;
     
     tria_mask5 = roipoly(gray,[size(gray,2)/4 3*size(gray,2)/4 size(gray,2)/2],[size(gray,1)/4 size(gray,1)/4 3*size(gray,1)/4]);
     tria_mask4 = roipoly(gray,[1 size(gray,2) size(gray,2)/2],[1 1 size(gray,1)])-tria_mask5;
     
+    if size(centersRedDark,1)==1
+        circ_mask7 = circularMask(centersRedDark,radiiRedDark+2,size(gray));       
+    else
+        circ_mask7 = circularMask(size(gray)/2,2*mean(size(gray))/3,size(gray));
+    end
+    
+    if size(centersRedBright,1)==1
+        circ_mask6 = circularMask(centersRedBright,radiiRedBright+2,size(gray)) - circ_mask7;       
+    else
+        circ_mask6 = circularMask(size(gray)/2,mean(size(gray))/2,size(gray)) - circ_mask7;
+    end
+    
+    %% SCORING 
+    
     score_blue1 = sum(sum(circ_mask1.*blueMask))/sum(sum(circ_mask1));
     score_white1 = sum(sum(circ_mask1.*whitishMask))/sum(sum(circ_mask1));
     
     score_red2 = sum(sum(tria_mask2.*redMask))/sum(sum(tria_mask2));
     score_red4 = sum(sum(tria_mask4.*redMask))/sum(sum(tria_mask4));
-        
+    
+    score_red6 = sum(sum(circ_mask6.*redMask))/sum(sum(circ_mask6));
+    score_red7 = sum(sum(circ_mask7.*redMask))/sum(sum(circ_mask7));
+    
+    %% DETECTION    
     result='dunno';
+    
+    %% DETECT MANDATORY
     if score_blue1 > 0.5 && score_white1 < 0.4 && score_red2 < 0.65 && score_red4 < 0.4
         if size(centersBlueBright,1)==1
             result='mandatory';
@@ -88,23 +116,39 @@ for k = 1 : length(theFiles)
         end
     end
     
+    %% DETECT PROHIBITORY TO BE IMPROVED
+    if score_red6 > 0.5 && score_red7 < 0.5
+        if size(centersRedBright,1)==1 && size(centersRedDark,1)==1
+            result = 'prohibitory';
+        elseif score_red6 > 0.6 && score_red7 < 0.6 && score_blue1 < 0.1 && score_white1 > 0.35
+            result = 'prohibitory';
+        end
+    end
+    
+    %% PRINT STUFF
+    
     gt_index = find(strcmp({ground_truth_2.filename}, baseFileName)==1);
     gt_name = ground_truth_2(gt_index).name;
     
     figure();imshow(original);
     % Plot bright circles in blue
-    %viscircles(centersBright, radiiBright,'Color','b');
+    viscircles(centersBright, radiiBright,'Color','g');
     % Plot dark circles in dashed red boundaries
-    %viscircles(centersDark, radiiDark,'LineStyle','--');
+    viscircles(centersDark, radiiDark,'LineStyle','--','Color','g');
     % Plot dark circles in dashed red boundaries
-    viscircles(centersBlueDark, radiiBlueDark,'LineStyle','--');
+    viscircles(centersBlueDark, radiiBlueDark,'LineStyle','--','Color','b');
     % Plot bright circles in blue
     viscircles(centersBlueBright, radiiBlueBright,'Color','b');
+    % Plot dark circles in dashed red boundaries
+    viscircles(centersRedDark, radiiRedDark,'LineStyle','--','Color','r');
+    % Plot bright circles in blue
+    viscircles(centersRedBright, radiiRedBright,'Color','r');
     
     if(strcmp(result, gt_name)==1)
         title(strcat('Correct! ',result,baseFileName));
     else
         title(strcat('\fontsize{16}\color{red}WRONG ',result,baseFileName))
+        fprintf(1, 'r6 %d r7 %d b %d w %d\n', score_red6, score_red7, score_blue1, score_white1);
     end
     
     
