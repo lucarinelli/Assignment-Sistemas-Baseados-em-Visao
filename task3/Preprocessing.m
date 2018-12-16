b7732e75ae3c0c189e8a67a2c32dea69c2463ace
Maicol Dolci <dolci95@hotmail.it>
2018-12-16 11:58:54 +0000

    Classification: area, centroid, Nobjects,minaxis,maxaxis

--- task3/Preprocessing.m
+++ task3/Preprocessing.m
@@ -4,15 +4,24 @@ function [bin_mask] = Preprocessing(Image)
 
 resized = imresize(Image,[400 400],'bilinear');  %resize
 resized2 = imsharpen(resized);
-gray = rgb2gray(resized2);
+deblurr = edgetaper(resized2,fspecial('gaussian',5,0.5));
+gray = rgb2gray(deblurr);
 gray_filt = medfilt2(gray);
 tria_mask2 = roipoly(gray,[size(gray,2)/2 1 size(gray,2)],[1 size(gray,1) size(gray,1)]);   
 [r c] = size(gray_filt);
-mean = mean2(gray_filt(100:300,100:300)); 
+meano = mean2(gray_filt(100:300,100:300)); 
 %if the image is too dark, it increases contrast  
-if mean <=40
+if meano <=60
     gray_filt = adapthisteq(gray_filt, 'ClipLimit' ,0.01,'NumTiles',[4 4]);     
-end   
+end  
+if meano >=210
+    for m=1:r
+        for l=1:c
+         gray_filt(m,l)= gray_filt(m,l) - 50;
+        end
+    end
+end
+    
         
  step1 = gray_filt;
  
@@ -31,5 +40,8 @@ end
     bin3 = imbinarize(resized_eq3); %binarization
     bin3_compl = imcomplement(bin3);
     bin_mask = bin3_compl.*tria_mask2;
+    bin_mask = imerode(bin_mask, strel('disk',2));
+    bin_mask = imerode(bin_mask, strel('disk',2));
+
     
 end
